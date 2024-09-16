bfaba533a83c3d248589fe1584690ae2e2ba1bd6
Levy Pinheiro <111375592+Levy3006@users.noreply.github.com>
2024-09-16 19:28:27 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,1 @@
-class Calculadora:
-    
-    def calcular(self,num1, num2,op):
-        if op == '+':
-            return self.__somar(num1,num2)
-        elif op == '-':
-            return self.__subtrair(num1,num2)
-        else:
-            return 'operação inválida'
-    def __somar(self, a, b):
-        return a+b
-    def __subtrair(self, a, b):
-        return a-b
-    
-calculadora = Calculadora()
-print(calculadora.calcular(1,2,'*'))
+
